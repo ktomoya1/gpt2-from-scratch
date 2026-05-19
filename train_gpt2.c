46965d9920c5762e049e41e7b1cbe8fcbbc40bd1
tom <134240277+ktomoya1@users.noreply.github.com>
2026-05-19 11:24:34 +0900

    Add ParameterTensors struct with comments

--- train_gpt2.c
+++ train_gpt2.c
@@ -6,3 +6,20 @@ typedef struct {
     int num_heads; // 並列で実行されるAttention関数の個数。
     int channels; // 各トークンのベクトルの要素数。
 } GPT2Config;
+
+typedef struct {
+    float* wte; // 埋め込み行列(V, C)、トークンIDをベクトルに変換する・logits計算前に次元をVに戻す役割を持つ
+    float* wpe; // 位置ベクトル(maxT, C)。トークン列を並列で処理するため、順序を持たない。そのため、位置情報を加える。
+    float* ln1w; // 一層目の正規化(V, C)。
+    float* ln1b; // バイアス項(V, C)。
+    float* qkvw; // 入力トークンからQKVを生成するための重み行列(T, 3*C, C)。
+    float* qkvb; // バイアス項(T, 3*C)。
+    float* attprojw; // h個のAttentionの出力を連結したものを重みづけする役割を持つ。(T, C)
+    float* attprojb; // バイアス項(T, C)
+    float* fcw; // FFN層の重み行列(T, 4*C)。次元を４倍に拡張し、表現力をあげる。
+    float* fcb; // バイアス項(T, 4*C)
+    float* fcprojw; // GeLU関数後に次元を元に戻す重み行列(T, C)。
+    float* fcprojb; // バイアス項(T, C)。
+    float* lnfw; // 最終層のLN（T, C)。加算され続けた分散のスケールを安定させる。
+    float* lnfb; // バイアス項(T, C)。
+} ParameterTensors;
